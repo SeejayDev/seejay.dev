472ba01e42bab53a4feaa2f6e590c23a6a1b6f22
SeejayDev <seejaydev@gmail.com>
2024-05-13 21:39:18 +0800

    Policy for ngauculator

---
+++ src/pages/privacy/ngauculator.tsx
@@ -0,0 +1,61 @@
+import React from "react";
+
+const ngauculator = () => {
+  return (
+    <div className="w-screen justify-center flex flex-col text-center">
+      <p className="text-center text-2xl font-bold">
+        Ngauculator Privacy Policy
+      </p>
+
+      <p>
+        Ngauculator is a private app developed for entertainment purposes ONLY.
+      </p>
+
+      <p>
+        Ngauculator is provided to users as-is with the developer releasing all
+        responsibilities regarding the usage and consequences of using the app.
+      </p>
+
+      <p>
+        The developer will not be responsible for any liabilities or loss due to
+        the use of the app.
+      </p>
+
+      <p>
+        The original source code uploaded to the Google Play Store DOES NOT
+        collect any personal or confidential information about the user.
+      </p>
+
+      <p>
+        The app only uses the device's local storage to store the user's
+        preferences related to the content of the app.
+      </p>
+
+      <p>This includes, but is not limited to:</p>
+      <ul>
+        <li>1. App color theme</li>
+        <li>2. App input method</li>
+        <li>3. Algorithm configuration</li>
+      </ul>
+
+      <p>
+        By using the app, users consent to allowing the app to store these
+        information onto their device's local storage system and memory.
+      </p>
+
+      <p>
+        All statements above are referring to the official download available
+        through the Google Play Store.
+      </p>
+
+      <p>
+        The developer does not claim any association with versions of the app
+        provided by other platforms or sources.
+      </p>
+
+      <p>This policy was last updated on the 13th of May 2024.</p>
+    </div>
+  );
+};
+
+export default ngauculator;
